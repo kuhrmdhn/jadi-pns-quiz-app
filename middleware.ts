ab8e24cb8d7f67d8e12fceb7c405dd1f319a44b0
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-02-21 09:31:29 +0700

    [fix] fix middleware flow

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    const authPage = ["/login", "/register"];
-    const protectedPage = ["/exercise"];
+    const protectedPages = ["/login", "/register","/exercise"];
     const token = req.cookies.get("firebase_token")?.value;
     const { pathname } = req.nextUrl;
 
-    if (token) {
-        return NextResponse.next();
+    if (protectedPages.includes(pathname) && token) {
+        return NextResponse.redirect(new URL("/", req.url))
     }
 
-    try {
-        const refreshTokenResponse = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/refresh-token`, {
-            method: "POST",
-            credentials: "include"
-        });
+    if (!token) {
+        try {
+            const refreshTokenResponse = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/verify-user`, {
+                method: "POST",
+                credentials: "include"
+            })
 
-        if (!refreshTokenResponse.ok) {
+            if (!refreshTokenResponse.ok) {
+                return NextResponse.redirect(new URL("/login", req.url));
+            }
+
+            return NextResponse.next()
+        } catch (error) {
+            console.error(error);
             return NextResponse.redirect(new URL("/login", req.url));
         }
-
-    } catch (error) {
-        console.error(error);
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    if (authPage.some((path) => pathname.startsWith(path)) && !token) {
-        return NextResponse.next();
-    }
-
-    if (protectedPage.some((path) => pathname.startsWith(path)) && !token) {
-        return NextResponse.redirect(new URL("/login", req.url));
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/", "/exercise/:path*", "/login", "/register"],
+    matcher: ["/exercise/:path*", "/login", "/register"],
 };
