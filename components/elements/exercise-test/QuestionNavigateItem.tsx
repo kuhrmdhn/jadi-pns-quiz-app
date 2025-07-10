b2df81d0d1ad23a21aa4a561f144762e5c097251
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-07-10 18:59:08 +0700

    [feature] add auto close question menu and menu item indicator

--- components/elements/exercise-test/QuestionNavigateItem.tsx
+++ components/elements/exercise-test/QuestionNavigateItem.tsx
@@ -1,18 +1,33 @@
-import { Button } from '@/components/ui/button'
+import { Button, ButtonProps, ButtonVariant } from '@/components/ui/button'
+import { useQuestionNavigationMenuStore } from '@/utils/store/useQuestionNavigationMenuStore'
 import { useUserExerciseAnswer } from '@/utils/store/useUserExerciseAnswer'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { RefAttributes, useEffect, useState } from 'react'
 
 type Props = {
     id: string
 }
 
 export default function QuestionNavigateItem({ id }: Props) {
+    const { closeMenu } = useQuestionNavigationMenuStore()
+    const [buttonVariant, setButtonVariant] = useState<ButtonVariant>("default")
     const { userAnswers } = useUserExerciseAnswer()
     const pathname = usePathname()
+    const searchParam = useSearchParams()
+    const currentQuestionIdParameter = searchParam.get("question_id")
+
+    useEffect(() => {
+        if (userAnswers[parseInt(id) - 1] == "null") {
+            setButtonVariant("outline")
+        }
+        if (currentQuestionIdParameter == id) {
+            setButtonVariant("secondary")
+        }
+    }, [])
     return (
-        <Button className='h-7 w-12' variant={userAnswers[parseInt(id) - 1] == "null" ? "outline" : "default"}>
-            <Link href={`${pathname}?question_id=${id}`}>
+        <Button onClick={closeMenu} className='h-7 w-12 p-0' variant={buttonVariant}>
+            <Link href={`${pathname}?question_id=${id}`} className='size-full flex justify-center items-center'>
                 {id}
             </Link>
         </Button>
--- components/elements/exercise-test/QuestionNavigateMenu.tsx
+++ components/elements/exercise-test/QuestionNavigateMenu.tsx
@@ -1,28 +1,29 @@
 "use client";
-import { useRef, useState } from "react";
+import useClickOutside from "@/utils/hooks/useClickOutside";
+import { useQuestionNavigationMenuStore } from "@/utils/store/useQuestionNavigationMenuStore";
+import { useRef } from "react";
 import { IoMdApps } from "react-icons/io";
 import QuestionNavigationList from "./QuestionNavigationList";
-import useClickOutside from "@/utils/hooks/useClickOutside";
 
 export default function QuestionNavigateMenu() {
-    const [isOpen, setIsOpen] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
+    const { menuOpen, closeMenu, toggleMenu } = useQuestionNavigationMenuStore()
 
     useClickOutside(
         menuRef,
-        () => setIsOpen(false)
+        () => closeMenu()
     );
 
     return (
         <div className="relative">
             <button
-                onClick={() => setIsOpen((prev) => !prev)}
+                onClick={() => toggleMenu()}
                 className="p-2 rounded-full bg-transparent"
             >
                 <IoMdApps size={44} />
             </button>
             {
-                isOpen && (
+                menuOpen && (
                     <section
                         ref={menuRef}
                         className="absolute top-12 left-0 shadow-lg p-4 rounded-lg w-96 h-fit bg-white dark:bg-black"
